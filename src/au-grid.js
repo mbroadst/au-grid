a8dce4e021e12cb4204b09828171e578155046fb
Matt Broadstone <mbroadst@gmail.com>
2016-04-18 15:33:46 -0400

    feat(au-grid): add index for plugin

---
+++ src/au-grid.js
@@ -0,0 +1,11 @@
+import {Grid} from './grid';
+import {Column} from './column';
+
+export function configure(aurelia) {
+  aurelia.globalResources(
+    './grid',
+    './column'
+  );
+}
+
+export { Grid, Column };
